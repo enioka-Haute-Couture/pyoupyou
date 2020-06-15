936f8075e6a9eb65f8ac56949f1abfdd94708ac8
Benjamin Port <benjamin.port@enioka.com>
2020-06-15 20:15:36 +0200

    Gantt: Remove range selector, not needed

--- interview/views.py
+++ interview/views.py
@@ -879,7 +879,7 @@ def gantt(request):
     fig = ff.create_gantt(
         processes_dict, index_col="ContractType", show_colorbar=True, showgrid_x=True, showgrid_y=True
     )
-    fig.layout.update({"title": {"text": _t("Contracts")}, "xaxis": {"rangeslider": {"visible": False}}})
+    fig.layout.update({"title": {"text": _t("Contracts")}, "xaxis": {"rangeslider": {"visible": False}, "rangeselector": None}})
 
     config = dict({"scrollZoom": False, "staticPlot": False, "showAxisRangeEntryBoxes": False, "displayModeBar": False})
 
