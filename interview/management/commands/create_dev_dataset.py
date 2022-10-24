03d7eb176d6bfbc6ee4025f28961225b15a73b44
Arnaud Baradat <arnaud.baradat@epita.fr>
2022-10-24 10:22:02 +0200

    Add create dataset command

---
+++ interview/management/commands/create_dev_dataset.py
@@ -0,0 +1,13 @@
+from django.core.management import BaseCommand
+
+"""
+Create some data and load them
+"""
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        pass
---
+++ interview/management/commands/delete_dataset.py
@@ -0,0 +1,13 @@
+from django.core.management import BaseCommand
+
+"""
+Delete old data
+"""
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        pass
