55e62a2f929d30cc43a3ae7959303a21a099fb46
Arnaud Baradat <arnaud.baradat@epita.fr>
2022-10-24 16:12:18 +0200

    Update create dataset: create subsidiary and some user for it

--- interview/management/commands/create_dev_dataset.py
+++ interview/management/commands/create_dev_dataset.py
@@ -1,7 +1,17 @@
 from django.core.management import BaseCommand
+from django.core.management import call_command
+
+from ref.factory import SubsidiaryFactory, PyouPyouUserFactory, ConsultantFactory
 
 """
 Create some data and load them
+
+Empty db: ./manage.py flush [--no-input]
+
+For now 2 subsidiaries
+            5 users per subsidiary
+            around 100 process per subsidiary in the last 2 years
+                an average of 3 itw per process
 """
 
 
@@ -10,4 +20,12 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        pass
+        call_command("delete_dataset")
+        subsidiary = SubsidiaryFactory(name="HauteCouture")
+        subsidiary_consultants = []
+
+        for _ in range(5):
+            subsidiary_consultants.append(ConsultantFactory(company=subsidiary))
+
+        subsidiary.responsible = subsidiary_consultants[0]
+        subsidiary.save()
