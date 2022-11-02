4362a0b45f99a941898f9984298e8e8774557635
Arnaud Baradat <arnaud.baradat@epita.fr>
2022-11-02 18:16:05 +0100

    Update refactor to have most of the generation inside factories

--- interview/factory.py
+++ interview/factory.py
@@ -1,14 +1,15 @@
 import datetime
-import random
+from factory.random import random
 
 import factory.fuzzy
-import factory.faker as faker
+from factory.faker import faker
 import pytz
 from dateutil.relativedelta import relativedelta
 
-from interview.models import Interview, Sources, SourcesCategory, ContractType, InterviewKind
+from interview.models import Interview, Sources, SourcesCategory, ContractType, InterviewKind, Process
 from ref.factory import SubsidiaryFactory
 from ref.models import Subsidiary, Consultant
+
 
 test_tz = pytz.timezone("Europe/Paris")
 
@@ -25,6 +26,39 @@ def date_random_plus_minus_time(date=None, years=0, months=0, weeks=0, days=0, t
     return date + (random.choice([-1, +1]) * relativedelta(years=years, months=months, weeks=weeks, days=days))
 
 
+def compute_next_planned_date(current_planned_date):
+    return date_random_plus_minus_time((current_planned_date + relativedelta(weeks=1)), days=5)
+
+
+def negative_end_process(process, itw, next_planned_date):
+    process.contract_start_date = next_planned_date + relativedelta(weeks=2)
+    process.end_date = next_planned_date
+    process.state = random.choices([Process.NO_GO, Process.CANDIDATE_DECLINED], weights=(80, 20), k=1)[0]
+
+    if process.state == Process.NO_GO:
+        itw.state = Interview.NO_GO
+    else:
+        # if candidate declined then we were a go
+        itw.state = Interview.GO
+
+    process.save()
+    itw.save()
+
+    return process, itw
+
+
+def get_available_consultants_for_itw(subsidiary, all_itw_given_process):
+    # retrieve available consultants that have not yet been involved in the process
+    possible_interviewer = Consultant.objects.filter(company=subsidiary).exclude(
+        id__in=list(all_itw_given_process.values_list("interviewers", flat=True))
+    )
+    # if all consultants were already involved in the process, choose one at random
+    if not possible_interviewer:
+        possible_interviewer = Consultant.objects.filter(company=subsidiary)
+
+    return possible_interviewer
+
+
 class CandidateFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "interview.Candidate"
@@ -34,7 +68,7 @@ class CandidateFactory(factory.django.DjangoModelFactory):
         lambda candidate: "{candidate_name}@mail.com".format(candidate_name=candidate.name.replace(" ", "_").lower())
     )
     phone = factory.LazyFunction(
-        lambda: "+336{random_digits}".format(random_digits=faker.faker.Faker().random_number(fix_len=True, digits=8))
+        lambda: "+336{random_digits}".format(random_digits=faker.Faker().random_number(fix_len=True, digits=8))
     )
 
 
@@ -67,6 +101,23 @@ class OfferFactory(factory.django.DjangoModelFactory):
 
     subsidiary = factory.SubFactory(SubsidiaryFactory)
 
+    @staticmethod
+    def create_processes(offer, subsidiary, min_number_of_process=9, max_number_of_process=12):
+        processes = []
+        for k in range(random.randrange(start=min_number_of_process, stop=max_number_of_process)):
+            process = ProcessFactory(
+                offer=offer,
+                subsidiary=subsidiary,
+                contract_type=random.choice(ContractType.objects.all()),
+                sources=random.choice(Sources.objects.all()),
+            )
+
+            process.responsible.set([subsidiary.responsible])
+            process.save()
+
+            processes.append(process)
+        return processes
+
 
 class ProcessFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -98,6 +149,77 @@ class ProcessFactory(factory.django.DjangoModelFactory):
 
     # (when process was created)
     start_date = factory.fuzzy.FuzzyDateTime(date_minus_time_ago(years=2))
+
+    @staticmethod
+    def create_interviews(process, subsidiary, min_number_of_itw=1, max_number_of_itw=5, start_date=None):
+        # will create interviews in the last two years until now (excluded)
+
+        number_of_itw = random.randrange(min_number_of_itw, max_number_of_itw)
+
+        process.start_date = start_date
+
+        # generate random date if start_date isn't specified
+        fake = faker.Faker()
+        if not start_date:
+            process.start_date = fake.date_time_between(
+                start_date=date_minus_time_ago(years=2, tz=test_tz),
+                end_date=date_minus_time_ago(weeks=(2 + 2 * number_of_itw), tz=test_tz),
+                tzinfo=test_tz,
+            )
+        process.save()
+
+        # first itw around a week after +|- 5 days
+        next_planned_date = compute_next_planned_date(current_planned_date=process.start_date)
+
+        # generate itws for process
+        for itw_number in range(number_of_itw):
+            all_itw = Interview.objects.filter(process=process)
+
+            itw = InterviewFactory(
+                process=process,
+                planned_date=next_planned_date,
+                kind_of_interview=random.choice(InterviewKind.objects.all()),
+            )
+            prequal = Interview.objects.filter(process=itw.process).values_list("rank", flat=True).last() == 1
+
+            # if this itw could be a prequal
+            if prequal:
+                # choose randomly if this interview is a prequal or not
+                prequal = random.choice([True, False])
+            itw.prequalification = prequal
+
+            # if there are more interview then the last ones were a GO
+            if itw_number + 1 < number_of_itw:
+                itw.state = Interview.GO
+            elif not next_planned_date >= datetime.datetime.now(tz=test_tz):
+                # by default all process end negatively
+                process, itw = negative_end_process(process=process, itw=itw, next_planned_date=next_planned_date)
+
+            # retrieve consultants to do the itw
+            possible_interviewer = get_available_consultants_for_itw(
+                subsidiary=subsidiary, all_itw_given_process=all_itw
+            )
+
+            # never more than 3 interviewers
+            number_of_interviewers = len(possible_interviewer) if len(possible_interviewer) < 4 else 4
+
+            # add one or more interviewers
+            itw.interviewers.set(
+                random.choices(
+                    possible_interviewer,
+                    k=random.randrange(1, number_of_interviewers) if number_of_interviewers > 1 else 1,
+                )
+            )
+            itw.save()
+
+            # if the next interview is in the future
+            if next_planned_date >= datetime.datetime.now(tz=test_tz):
+                itw.state = random.choice([Interview.WAITING_PLANIFICATION_RESPONSE, Interview.PLANNED])
+                itw.save()
+                break  # stop creating new interviews for process as this one hasn't happened yet
+
+            # compute next planned date
+            next_planned_date = compute_next_planned_date(current_planned_date=next_planned_date)
 
 
 class InterviewKindFactory(factory.django.DjangoModelFactory):
--- interview/management/commands/create_dev_dataset.py
+++ interview/management/commands/create_dev_dataset.py
@@ -24,7 +24,11 @@ from interview.factory import (
 from interview.models import ContractType, SourcesCategory, InterviewKind, Interview, Process, Sources
 from ref.factory import SubsidiaryFactory, PyouPyouUserFactory, ConsultantFactory
 from ref.models import Consultant
-from interview.factory import date_minus_time_ago, date_random_plus_minus_time, test_tz
+from interview.factory import (
+    date_minus_time_ago,
+    date_random_plus_minus_time,
+    test_tz,
+)
 
 """
 Create some data and load them
@@ -61,23 +65,6 @@ def generate_basic_data(subsidiary):
         SourcesFactory(category=category, name=subsidiary.name)
 
 
-def negative_end_process(process, itw, next_planned_date):
-    process.contract_start_date = next_planned_date + relativedelta(weeks=2)
-    process.end_date = next_planned_date
-    process.state = random.choices([Process.NO_GO, Process.CANDIDATE_DECLINED], weights=(80, 20), k=1)[0]
-
-    if process.state == Process.NO_GO:
-        itw.state = Interview.NO_GO
-    else:
-        # if candidate declined then we were a go
-        itw.state = Interview.GO
-
-    process.save()
-    itw.save()
-
-    return process, itw
-
-
 def set_random_process_given_offer_to_hired(offer):
     process_hired = random.choice(Process.objects.filter(offer=offer))
     process_hired.state = Process.HIRED
@@ -87,24 +74,6 @@ def set_random_process_given_offer_to_hired(offer):
     last_itw = Interview.objects.filter(process=process_hired).last()
     last_itw.state = Interview.GO
     last_itw.save()
-
-
-def compute_next_planned_date(current_planned_date):
-    return date_random_plus_minus_time((current_planned_date + relativedelta(weeks=1)), days=5)
-
-
-def get_available_consultants_for_itw(subsidiary, all_itw_given_process):
-    # retrieve available consultants that have not yet been involved in the process
-    possible_interviewer = (
-        Consultant.objects.filter(subsidiary=subsidiary)
-        .exclude(id__in=all_itw_given_process.values_list("interviewers", flat=True))
-        .distinct()
-    )
-    # if all consultants were already involved in the process, choose one at random
-    if not possible_interviewer:
-        possible_interviewer = Consultant.objects.filter(subsidiary=subsidiary)
-
-    return possible_interviewer
 
 
 class Command(BaseCommand):
@@ -132,151 +101,25 @@ class Command(BaseCommand):
                 subsidiary_offers.append(OfferFactory(subsidiary=subsidiary))
 
             # for each offer create some process for it
-            processes_given_offer = []
             for offer in subsidiary_offers:
-                processes = []
-                for k in range(random.randrange(start=9, stop=12)):
-                    process = ProcessFactory(
-                        offer=offer,
-                        subsidiary=subsidiary,
-                        contract_type=random.choice(ContractType.objects.all()),
-                        sources=random.choice(Sources.objects.all()),
-                    )
+                processes = OfferFactory.create_processes(offer=offer, subsidiary=subsidiary)
 
-                    process.responsible.set([subsidiary.responsible])
-                    process.save()
-
-                    processes.append(process)
-                processes_given_offer.append(processes)
-
-            # for each process create some itw
-            for processes in processes_given_offer:
+                # for each process create some itw
                 for process in processes:
-                    number_of_itw = random.randrange(1, 5)
+                    ProcessFactory.create_interviews(process=process, subsidiary=subsidiary)
 
-                    # generate random date
-                    fake = faker.Faker()
-                    process.start_date = fake.date_time_between(
-                        start_date=date_minus_time_ago(years=2, tz=test_tz),
-                        end_date=date_minus_time_ago(weeks=(2 + 2 * number_of_itw), tz=test_tz),
-                        tzinfo=test_tz,
-                    )
-                    process.save()
-
-                    # first itw around a week after +|- 5 days
-                    next_planned_date = compute_next_planned_date(current_planned_date=process.start_date)
-
-                    # generate itws for process
-                    for itw_number in range(number_of_itw):
-                        all_itw = Interview.objects.filter(process=process)
-
-                        itw = InterviewFactory(
-                            process=process,
-                            planned_date=next_planned_date,
-                            kind_of_interview=random.choice(InterviewKind.objects.all()),
-                        )
-                        prequal = (
-                            Interview.objects.filter(process=itw.process).values_list("rank", flat=True).last() == 1
-                        )
-
-                        # if this itw could be a prequal
-                        if prequal:
-                            # choose randomly if this interview is a prequal or not
-                            prequal = random.choice([True, False])
-                        itw.prequalification = prequal
-
-                        # if there are more interview then the last ones were a GO
-                        if itw_number + 1 < number_of_itw:
-                            itw.state = Interview.GO
-                        else:
-                            # by default all process end negatively
-                            process, itw = negative_end_process(
-                                process=process, itw=itw, next_planned_date=next_planned_date
-                            )
-
-                        # retrieve consultants to do the itw
-                        possible_interviewer = get_available_consultants_for_itw(
-                            subsidiary=subsidiary, all_itw_given_process=all_itw
-                        )
-
-                        # never more than 3 interviewers
-                        number_of_interviewers = len(possible_interviewer) if len(possible_interviewer) < 4 else 4
-
-                        # add one or more interviewers
-                        itw.interviewers.set(
-                            random.choices(
-                                possible_interviewer,
-                                k=random.randrange(1, number_of_interviewers) if number_of_interviewers > 1 else 1,
-                            )
-                        )
-                        itw.save()
-
-                        # compute next planned date
-                        next_planned_date = compute_next_planned_date(current_planned_date=next_planned_date)
-
-            # set one process of each offer to Hired
-            for o in subsidiary_offers:
-                set_random_process_given_offer_to_hired(offer=o)
+                # set one process of each offer to Hired
+                set_random_process_given_offer_to_hired(offer=offer)
 
             # create an offer sill ongoing
             pending_offer = OfferFactory(subsidiary=subsidiary)
-            pending_processes = []
-
-            # create some processes for it
-            for k in range(random.randrange(start=5, stop=10)):
-                process = ProcessFactory(
-                    offer=pending_offer,
-                    subsidiary=subsidiary,
-                    contract_type=random.choice(ContractType.objects.all()),
-                    sources=random.choice(Sources.objects.all()),
-                )
-
-                process.responsible.set([subsidiary.responsible])
-                process.save()
-
-                pending_processes.append(process)
+            pending_processes = OfferFactory.create_processes(
+                offer=pending_offer, subsidiary=subsidiary, min_number_of_process=5, max_number_of_process=10
+            )
 
             # create some itw for each process
             for process in pending_processes:
-                number_of_itw = random.randrange(2, 5)
-                process.start_date = date_minus_time_ago(weeks=3) + relativedelta(days=random.randrange(3, 10))
-                process.save()
-
-                next_planned_date = compute_next_planned_date(current_planned_date=process.start_date)
-
-                for itw_number in range(number_of_itw):
-                    # get all past interviews for given process
-                    all_itw = Interview.objects.filter(process=process)
-
-                    itw = InterviewFactory(
-                        process=process,
-                        planned_date=next_planned_date,
-                        kind_of_interview=random.choice(InterviewKind.objects.all()),
-                    )
-
-                    # if there are more interview then the last ones were a GO
-                    if itw_number + 1 < number_of_itw:
-                        itw.state = Interview.GO
-                    # if it's the last interview but current itw isn't in the future
-                    elif next_planned_date < datetime.datetime.now(tz=test_tz):
-                        process, itw = negative_end_process(
-                            process=process, itw=itw, next_planned_date=next_planned_date
-                        )
-
-                    # retrieve consultants to do the itw
-                    possible_interviewer = get_available_consultants_for_itw(
-                        subsidiary=subsidiary, all_itw_given_process=all_itw
-                    )
-
-                    # set interviewers for itw
-                    itw.interviewers.add(random.choice(possible_interviewer))
-                    itw.save()
-
-                    # if the next interview is in the future
-                    if next_planned_date >= datetime.datetime.now(tz=test_tz):
-                        itw.state = random.choice([Interview.WAITING_PLANIFICATION_RESPONSE, Interview.PLANNED])
-                        itw.save()
-                        break  # stop creating new interviews for process as this one hasn't happened yet
-
-                    # compute next planned date
-                    next_planned_date = compute_next_planned_date(current_planned_date=next_planned_date)
+                start_date = date_minus_time_ago(weeks=3) + relativedelta(days=random.randrange(3, 10))
+                ProcessFactory.create_interviews(
+                    process=process, subsidiary=subsidiary, min_number_of_itw=2, start_date=start_date
+                )
