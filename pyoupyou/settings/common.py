952f135a404009b5be19716d46ab8fc107830797
Arnaud Baradat <arnaud.baradat@enioka.com>
2022-12-02 15:43:27 +0100

    #224 add global subsidiary filter

--- pyoupyou/settings/common.py
+++ pyoupyou/settings/common.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "pyoupyou.middleware.ExternalCheckMiddleware",
+    "pyoupyou.middleware.GlobalSubsidiaryFilterMiddleware",
 ]
 
 ROOT_URLCONF = "pyoupyou.urls"
