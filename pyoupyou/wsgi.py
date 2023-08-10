95dddc7f9f5d909a4e9646ce78dc3e8479b01261
Benjamin Port <benjamin.port@enioka.com>
2023-08-10 13:27:18 +0200

    Allow to setup PYOUPYOU_ENV from a file
    Fix #312

--- pyoupyou/wsgi.py
+++ pyoupyou/wsgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+def get_pyoupyou_env_from_file():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), ".pyoupyou_env")) as f:
+            return f.read().strip()
+    except Exception:
+        return ""
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyoupyou.settings")
+os.environ.setdefault("PYOUPYOU_ENV", get_pyoupyou_env_from_file())
 
 application = get_wsgi_application()
