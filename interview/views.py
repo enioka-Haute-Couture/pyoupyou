0640d425fc4d02a6a40e49e650f2f1b09c1f9c2c
Augustin Bessette <augustin.bessette@gmail.com>
2023-10-17 14:45:19 +0200

    Analysis processes and interviewes reports setup

--- interview/views.py
+++ interview/views.py
@@ -39,6 +39,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.dateparse import parse_date
 from django.db.models import Count
 from rest_framework.decorators import api_view
+from dateutil import parser
 
 from interview.decorators import privilege_level_check
 from interview.filters import (
@@ -947,14 +948,13 @@ def process_stats(process):
     if process.end_date:
         end_date = process.end_date
     else:
-        last_interview = Interview.objects.filter(process=process).order_by("planned_date").last()
-        if last_interview is None or last_interview.planned_date is None:
-            end_date = datetime.datetime.now().date()
+        if process.interview_last_planned_date:
+            end_date = process.interview_last_planned_date.date()
         else:
-            end_date = last_interview.planned_date.date()
+            end_date = datetime.datetime.now().date()
 
     process_length = (end_date - process.start_date).days
-    process_interview_count = Interview.objects.filter(process=process).count()
+    process_interview_count = process.interview_set.count()
 
     return process_length, process_interview_count
 
@@ -962,7 +962,11 @@ def process_stats(process):
 @login_required
 @require_http_methods(["GET"])
 def export_processes_tsv(request):
-    processes = Process.objects.for_user(request.user).prefetch_related("interview_set")
+    processes = (
+        Process.objects.for_user(request.user)
+        .prefetch_related("interview_set")
+        .annotate(interview_last_planned_date=Max("interview__planned_date"))
+    )
 
     ret = []
 
@@ -1031,6 +1035,7 @@ def export_interviews_tsv(request):
         .select_related("process__subsidiary")
         .select_related("process__offer")
         .prefetch_related(Prefetch("interviewers", queryset=consultants))
+        .annotate(interview_last_planned_date=Max("interview__planned_date"))
     )
 
     ret = []
@@ -1738,3 +1743,140 @@ def process_from_cognito_form(request, source_id, subsidiary_id):
     serializer.create(serializer.validated_data)
 
     return HttpResponse("Success")
+
+
+@login_required
+@user_passes_test(lambda u: not u.consultant.is_external)
+def interviews_pivotable(request):
+    data = []
+
+    subsidiary_filter = get_global_filter(request)
+
+    processes = subsidiary_filter.filter_queryset(
+        Process.objects.all()
+        .select_related("sources__category")
+        .select_related("contract_type")
+        .select_related("candidate")
+        .select_related("subsidiary")
+        .select_related("offer__subsidiary")
+        .prefetch_related("interview_set__kind_of_interview", "interview_set__interviewers__user")
+        .annotate(interview_last_planned_date=Max("interview__planned_date"))
+    )
+
+    # Analysis default filter on the current financial year
+    current_financial_year_default_filter = "%d/%d" % (datetime.date.today().year, datetime.date.today().year + 1)
+    financial_closing_current_date = parser.parse(
+        "%d-%s" % (datetime.date.today().year, settings.FINANCIAL_STARTING_MONTH_DAY)
+    ).date()
+    if datetime.date.today() < financial_closing_current_date:
+        current_financial_year_default_filter = "%d/%d" % (datetime.date.today().year - 1, datetime.date.today().year)
+
+    processes_length = {}
+    processes_itw_count = {}
+    for process in processes:
+        # Compute interview lenght
+        if process.id not in processes_length:
+            process_length, process_interview_count = process_stats(process)
+            processes_length[process.id] = process_length
+            processes_itw_count[process.id] = process_interview_count
+        else:
+            process_length = processes_length[process.id]
+            process_interview_count = processes_itw_count[process.id]
+
+        # Compute process start/end financial closing date
+        financial_closing_start_date = parser.parse(
+            "%d-%s" % (process.start_date.year, settings.FINANCIAL_STARTING_MONTH_DAY)
+        ).date()
+        process_start_fiscal_year = "%d/%d" % (process.start_date.year, process.start_date.year + 1)
+        if process.start_date < financial_closing_start_date:
+            process_start_fiscal_year = "%d/%d" % (process.start_date.year - 1, process.start_date.year)
+
+        process_end_fiscal_year = ""
+        if process.end_date is not None:
+            financial_closing_end_date = parser.parse(
+                "%d-%s" % (process.end_date.year, settings.FINANCIAL_CLOSING_MONTH_DAY)
+            ).date()
+            process_end_fiscal_year = "%d/%d" % (process.end_date.year, process.end_date.year + 1)
+            if process.end_date < financial_closing_end_date:
+                process_end_fiscal_year = "%d/%d" % (process.end_date.year - 1, process.end_date.year)
+
+        process_end_date = "" if process.end_date is None else process.end_date.strftime("%Y-%m-%d")
+        process_end_date_year_month = "" if process.end_date is None else process.end_date.strftime("%Y-%m")
+        process_end_date_year = "" if process.end_date is None else process.end_date.strftime("%Y")
+        process_source_category = "" if process.sources is None else process.sources.category.name
+        process_contract_start_date = (
+            "" if process.contract_start_date is None else process.contract_start_date.strftime("%Y-%m")
+        )
+        process_contract_duration = 0 if process.contract_duration is None else process.contract_duration
+        process_mean_days_between_itws = (
+            0 if process_interview_count == 0 else int(process_length / process_interview_count)
+        )
+
+        for idx, interview in enumerate(process.interview_set.all()):
+            interviewers = ""
+            for i in interview.interviewers.all():
+                interviewers += i.user.trigramme + "_"
+            interviewers = interviewers[:-1]
+
+            # Compute time elapsed since last event (previous interview or beginning of process)
+            time_since_last_is_sound = True
+            last_event_date = process.start_date
+            next_event_date = None
+            if interview.rank > 1:
+                last_itw = process.interview_set.all()[idx - 1]
+                if last_itw and last_itw.planned_date is not None:
+                    last_event_date = last_itw.planned_date.date()
+                else:
+                    time_since_last_is_sound = False
+            if interview.planned_date is None:
+                time_since_last_is_sound = False
+            else:
+                next_event_date = interview.planned_date.date()
+            if time_since_last_is_sound:
+                time_since_last_event = int((next_event_date - last_event_date).days)
+                # we have some processes that were created after the first itw was planned
+                if time_since_last_event < 0:
+                    time_since_last_event = _("Unknown")
+            else:
+                time_since_last_event = _("Unknown")
+
+            interview_planned_date = "" if interview.planned_date is None else interview.planned_date.strftime("%Y-%m")
+
+            data.append(
+                {
+                    _("subsidiary"): str(process.subsidiary),
+                    _("process start date"): process.start_date.strftime("%Y-%m-%d"),
+                    _("process start year month"): process.start_date.strftime("%Y-%m"),
+                    _("process start year"): process.start_date.strftime("%Y"),
+                    _("process start fiscal year"): process_start_fiscal_year,
+                    _("process end date"): process_end_date,
+                    _("process end year-month"): process_end_date_year_month,
+                    _("process end year"): process_end_date_year,
+                    _("process end fiscal year"): process_end_fiscal_year,
+                    _("process length"): process_length,
+                    _("source"): str(process.sources),
+                    _("source category"): process_source_category,
+                    _("offer"): str(process.offer),
+                    _("contract type"): str(process.contract_type),
+                    _("contract start date"): process_contract_start_date,
+                    _("contract duration"): process_contract_duration,
+                    _("process state label"): process.get_state_display(),
+                    _("process itw count"): process_interview_count,
+                    _("mean days between itws"): process_mean_days_between_itws,
+                    _("itw state label"): interview.get_state_display(),
+                    _("interviewers"): interviewers,
+                    _("interview rank"): interview.rank,
+                    _("days since last event"): time_since_last_event,
+                    _("itw planned date"): interview_planned_date,
+                    _("itw kind"): str(interview.kind_of_interview),
+                }
+            )
+
+    return render(
+        request,
+        "interview/pivotable.html",
+        {
+            "data": data,
+            "current_financial_year_default_filter": current_financial_year_default_filter,
+        },
+    )
